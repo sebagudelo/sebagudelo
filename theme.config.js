a0e26f637109a962be7e2ca953c0ce872bcd15fb
sebagudelo <43288271+sebagudelo@users.noreply.github.com>
2021-10-04 11:16:14 +1100

    updated theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{2021}</time> © Seb Agudelo.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
