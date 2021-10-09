f9ea641e4c18235bf9b38085325ade486dd109a5
sebagudelo <43288271+sebagudelo@users.noreply.github.com>
2021-10-09 11:40:00 +1100

    Adding GTM NPM Module

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,14 @@ import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
 
 import '../styles/main.css'
+import TagManager from 'react-gtm-module'
+ 
+const tagManagerArgs = {
+    gtmId: 'GTM-KFTWFLX'
+}
+ 
+TagManager.initialize(tagManagerArgs)
+
 
 export default function Nextra({ Component, pageProps }) {
   return (
