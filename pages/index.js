bb698636cffdc616a4804d76462e899810541160
sebagudelo <43288271+sebagudelo@users.noreply.github.com>
2021-11-13 15:21:31 +1100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+import SignupForm from '../components/SignupForm';
+
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hey, I'm Seb Agudelo. Head of Marketing @ {' '}
+          <a href="https://nhost.io">Nhost</a></p>
+        <p>I write about Growth, Startups and The Web.</p>
+        <p>Subscribe to get updates on building marketing from zero to one.</p>
+      </section>
+
+      <section>
+      <SignupForm/>
+      </section>
+      
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+                </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
