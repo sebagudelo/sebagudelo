75cf9d421293970a0455000715e00d15df085a00
sebagudelo <43288271+sebagudelo@users.noreply.github.com>
2022-02-12 13:15:50 +1100

    updated og metadata with cards

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "92e9cb6587add187"; }
+/******/ 		__webpack_require__.h = function() { return "cdd857c499e0b6a1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.e3b308cbb0caa8f9.hot-update.js
+++ .next/static/webpack/webpack.e3b308cbb0caa8f9.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3dd09758c435b905"; }
+/******/ 	__webpack_require__.h = function() { return "68b17d4aeb3a5a00"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
